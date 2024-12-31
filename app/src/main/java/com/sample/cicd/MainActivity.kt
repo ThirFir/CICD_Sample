ed90ba76a7ffec01671e18d4afc7ad47a7652ebc
Thirfir <sangbbang31@gmail.com>
2024-12-31 10:18:20 +0900

    Test build ci

--- app/src/main/java/com/sample/cicd/MainActivity.kt
+++ app/src/main/java/com/sample/cicd/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.sample.cicd.ui.theme.CICD_SampleTheme
 
@@ -22,11 +21,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             CICD_SampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = "CICD SAMPLE",
-                        fontSize = 40.sp,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier.padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "CICD SAMPLE",
+                            fontSize = 40.sp,
+                        )
+                    }
                 }
             }
         }
